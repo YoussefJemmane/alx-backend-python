74f5354e803fa9c5d24c00e55b0836df566d7db8
YoussefJemmane <yous.jemm@gmail.com>
2025-06-14 19:09:54 +0100

    fix

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -15,7 +15,7 @@ def message_list(request):
     """
     Display all unread messages with edit history indicators.
     """
-    unread_messages = Message.unread.unread_for_user(request.user)
+    unread_messages = Message.unread.unread_for_user(request.user).select_related('sender', 'receiver').only('id', 'content', 'timestamp', 'sender__username', 'receiver__username')
     
     context = {
         'messages': unread_messages,
